2c0c6561d6494634b9c90efee8fc0cc84eea0a3d
Abhishek Dadwal <dadwalabhishek10@gmail.com>
2025-10-03 11:31:20 +0530

    grpc server added

---
+++ backend/analytics/main.go
@@ -0,0 +1,58 @@
+// analytics/main.go
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"net"
+
+	pb "event-analytics/proto/event-analytics/proto"
+
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedAnalyticsServiceServer
+	db *sql.DB
+}
+
+func (s *server) GetEventCount(ctx context.Context, req *pb.EventCountRequest) (*pb.EventCountResponse, error) {
+	var count int64
+	err := s.db.QueryRow(`
+        SELECT click_count FROM page_clicks 
+        WHERE user_id = $1 AND page_url = $2
+    `, req.UserId, req.PageUrl).Scan(&count)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.EventCountResponse{
+		Count:   count,
+		UserId:  req.UserId,
+		PageUrl: req.PageUrl,
+	}, nil
+}
+
+func main() {
+	// DB connection
+	db, err := sql.Open("postgres", "postgres://user:pass@localhost:5432/events?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// gRPC server
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterAnalyticsServiceServer(grpcServer, &server{db: db})
+
+	log.Println("Analytics service listening on :50051")
+	grpcServer.Serve(lis)
+}
